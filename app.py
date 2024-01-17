eb96bf20af29dee15f368601f54218810d8fd979
matgtm <40611532+matgtm@users.noreply.github.com>
2024-01-17 17:11:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -18,8 +18,12 @@ st.set_page_config(
 #url = 'http://127.0.0.1:8000'
 # load_dotenv()
 #url = os.getenv('API_URL')
-url = 'https://docker-test-pcb-cfiqqvqtva-uc.a.run.app'
 
+## Selecionar url de google cada uno (Diego, Mateo, Matias)
+#url = 'https://docker-test-pcb-cfiqqvqtva-uc.a.run.app' #MATIAS: Imagen sin conf thres customizable
+url = 'https://docker-test-pcb-cfiqqvqtva-uw.a.run.app'  #MATIAS con conf thres
+#url = ''   #MATEO
+#url = ''   #DIEGO
 
 # App title and description
 st.header('Detección de defectos en tarjetas PCB')
@@ -29,7 +33,7 @@ st.subheader('Umbral de confianza')
 
 conf = st.slider('Seleccionar confianza del modelo: ',min_value=0.10,max_value=1.00,step=0.05,value=0.25)
 
-st.write('Los defectos aparecerán cuando el modelo tenga un ',conf*100,'% de seguridad de que sean correctos.')
+st.write('El modelo solo identificará aquellos defectos para los que tenga al menos un ',conf*100,'% de seguridad en sus predicciones.')
 
 ### Create a native Streamlit file upload input
 st.markdown("### Cargar la foto de la tarjeta")
@@ -49,11 +53,11 @@ if img_file_buffer is not None:
       img_bytes = img_file_buffer.getvalue()
 
       ### Make request to  API (stream=True to stream response as bytes)
-      res = requests.post(url + "/upload_image", files={'img': img_bytes})
+      res = requests.post(url + "/upload_image", files={'img': img_bytes},params={'conf':conf})
 
       if res.status_code == 200:
         ### Display the image returned by the API
-        st.image(res.content, caption="Image returned from API ☝️")
+        st.image(res.content, caption="Imagen devuelta por API ☝️")
       else:
         st.markdown("**Oops**, something went wrong 😓 Please try again.")
         print(res.status_code, res.content)
